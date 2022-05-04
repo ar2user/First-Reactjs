a0eb658ed5a45c6629af0c0756cf7750b705ba8f
Arturo <arpego2@gmail.com>
2022-05-04 10:36:46 -0500

    Subiendo archivos 2

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+const nombre = 'Arturo';
+const apellido = 'Pérez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log ( nombre, apellido );
+
+if ( true ) {
+    const nombre = 'Arthur';
+    const apellido = 'González';
+    let valorDado = 6;
+    console.log ( nombre, apellido, valorDado );
+}
+
+console.log( valorDado );
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,12 @@
+const nombre = 'Arturo';
+const apellido = 'Pérez';
+
+let nombreCompleto = `${nombre} ${apellido}`;
+
+console.log( nombreCompleto );
+
+function getSaludo (nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log ( `Este es un texto: ${getSaludo( nombre )}` );
