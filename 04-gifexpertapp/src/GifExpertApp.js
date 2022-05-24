7c415b18838fa8cd2cd98aa4b3df33865d345d8d
Arturo <arpego2@gmail.com>
2022-05-24 17:43:45 -0500

    Ejercicio de useState

--- 04-gifexpertapp/src/GifExpertApp.js
+++ 04-gifexpertapp/src/GifExpertApp.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+//const categories = ['Cars', 'Games', 'Music'];
+
+const [categories, setCategories] = useState(['Cars', 'Games', 'Music']);
+
+const addCategory = () => {
+  setCategories( cats => ['landscape', ...cats]); 
+};
+
   return (
     <>
     <h2>GifExpertApp</h2>
     <hr></hr>
+    <button onClick={ addCategory }> Add Landscape </button>
+    <ul>
+        { categories.map( category =>{ return <li key={ category } > { category } </li> }) }
+    </ul>
     </>
   )
 }
